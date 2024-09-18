7c1e402068d119f0e2183458d7903a9b54e8ee92
DamianL96 <damianlambrecht@gmail.com>
2024-09-18 13:14:49 -0300

    componente grafico de dona, se instalo libreria chart.js 4.4.4

--- src/app/components/common/grafico-ingresos/grafico-ingresos.component.ts
+++ src/app/components/common/grafico-ingresos/grafico-ingresos.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Chart, ChartType } from 'chart.js/auto';
+import { response } from 'express';
 
 @Component({
   selector: 'app-grafico-ingresos',
@@ -9,6 +10,8 @@ import { Chart, ChartType } from 'chart.js/auto';
   styleUrl: './grafico-ingresos.component.css'
 })
 export class GraficoIngresosComponent implements OnInit{
+
+  @Input() valores!:number[];
 
   public chart?: Chart;
 
@@ -22,8 +25,8 @@ export class GraficoIngresosComponent implements OnInit{
         'Regalos'
       ],
       datasets: [{
-        label: 'My First Dataset',
-        data: [300, 50, 100, 20, 150],
+        label: 'Ingreso',
+        data: this.valores,
         backgroundColor: [
           'rgb(196, 48, 43)',
           'rgb(100, 65, 165)',
@@ -35,9 +38,23 @@ export class GraficoIngresosComponent implements OnInit{
       }]
     };
 
+    const options= {
+      responsive:true,
+      maintainAspectRatio: false,
+      layout:{
+        padding:5
+      },
+      plugins:{
+        legend:{
+          position: 'right' as const
+        }
+      }
+    };
+
     this.chart = new Chart("chart",{
       type:'doughnut' as ChartType,
       data: data,
+      options:options
     });
   }
 
