c2bcf30465af25ba24e89be6473e1d2c3d12ac86
Erick Romero <erickromeronieto018@gmail.com>
2024-09-24 14:24:33 -0500

    eliminé la importar de register de app.routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,6 @@
 import { Routes } from '@angular/router';
 import { LandingComponent } from './pages/landing/landing.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
-import { RegisterComponent } from './auth/layout/register/register.component'
-
 
 
 export const routes: Routes = [
