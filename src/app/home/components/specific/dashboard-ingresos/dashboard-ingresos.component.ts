c3652983a9f59a128c377fe370615a30f33808c9
DamianL96 <damianlambrecht@gmail.com>
2024-09-25 18:43:42 -0300

    selector de graficos implementado

--- src/app/home/components/specific/dashboard-ingresos/dashboard-ingresos.component.ts
+++ src/app/home/components/specific/dashboard-ingresos/dashboard-ingresos.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { GraficoIngresosComponent } from "../../common/grafico-ingresos/grafico-ingresos.component";
 import { TablaIngresosComponent } from "../../common/tabla-ingresos/tabla-ingresos.component";
 import { GraficoLinealComponent } from "../../common/grafico-lineal/grafico-lineal.component";
+import { SwapGraphicsComponent } from "../swap-graphics/swap-graphics.component";
 
 @Component({
   selector: 'app-dashboard-ingresos',
   standalone: true,
-  imports: [GraficoIngresosComponent, TablaIngresosComponent, GraficoLinealComponent],
+  imports: [GraficoIngresosComponent, TablaIngresosComponent, GraficoLinealComponent, SwapGraphicsComponent],
   templateUrl: './dashboard-ingresos.component.html',
   styleUrl: './dashboard-ingresos.component.css'
 })
