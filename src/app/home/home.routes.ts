a3d6f94463efe914965e37eeeb9071daf05743e7
Erick Romero <erickromeronieto018@gmail.com>
2024-09-24 21:14:20 -0500

    agregué maqueta rectangulos para precio y grafico

--- src/app/home/home.routes.ts
+++ src/app/home/home.routes.ts
@@ -1,7 +1,5 @@
 import { Routes } from "@angular/router";
 import { HomeComponent } from "./pages/home/home.component";
-import { DashboardComponent } from "./layout/dashboard/dashboard.component";
-import { CollaboratorPaymentsComponent } from "./layout/collaborator-payments/collaborator-payments.component";
 
 export const HOME_ROUTES: Routes = [
   {
@@ -10,7 +8,8 @@ export const HOME_ROUTES: Routes = [
     children: [
       { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
       { path: 'dashboard', loadComponent: () => import('./layout/dashboard/dashboard.component').then(c => c.DashboardComponent) },
-      { path: 'payments', loadComponent: () => import('./layout/collaborator-payments/collaborator-payments.component').then(c => c.CollaboratorPaymentsComponent) }
+      { path: 'payments', loadComponent: () => import('./layout/collaborator-payments/collaborator-payments.component').then(c => c.CollaboratorPaymentsComponent) },
+      { path: 'income', loadComponent: () => import('./layout/income/income.component').then(c => c.IncomeComponent) },
 
     ]
   }
