d7de944636156d17408b3e2271b88f0046249420
SuelenGalhardo <suelenranucci@gmail.com>
2024-09-27 19:46:04 +0200

    component tax impuestos y obligaciones

---
+++ src/app/home/layout/tax-obligations-component/tax-obligations-component.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tax-obligations-component',
+  standalone: true,
+  imports: [],
+  templateUrl: './tax-obligations-component.component.html',
+  styleUrl: './tax-obligations-component.component.css'
+})
+export class TaxObligationsComponentComponent {
+
+}
