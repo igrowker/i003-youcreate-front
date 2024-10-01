12299352b936c8f72fd352f44aef818b2a8610fc
Erick Romero <erickromeronieto018@gmail.com>
2024-09-30 22:25:29 -0500

    agregué validaciones del formulario

--- src/app/home/components/register-payments-dialog/register-payments-dialog.component.ts
+++ src/app/home/components/register-payments-dialog/register-payments-dialog.component.ts
@@ -1,7 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormGroup, ReactiveFormsModule, ValidationErrors, Validators } from '@angular/forms';
 import { RegisterPayments } from '../../../core/models/register-payments.interface';
+import { ErrorType } from '../../../core/models/error-type.interface';
 
 @Component({
   selector: 'register-payments-dialog',
@@ -14,6 +15,12 @@ export class RegisterPaymentsDialogComponent implements OnInit{
 
   public isVisible = false;
   public registerPaymentsForm!: FormGroup;
+  public paymentMethod:string[] = [
+    'Transferencia Bancaria',
+    'Tarjeta De Crédito',
+    'Tarjeta De Debito',
+    'Otro'
+  ]
 
   @Output() formValues = new EventEmitter<RegisterPayments>();
 
@@ -23,9 +30,9 @@ export class RegisterPaymentsDialogComponent implements OnInit{
       this.registerPaymentsForm = this.fb.group({
         name: ['', [Validators.required]],
         wallet: ['', [Validators.required]],
-        date: ['', [Validators.required]],
+        date: ['', [Validators.required, this.validDateValidator]],
         service: ['', [Validators.required]],
-        monto: [0, [Validators.required]],
+        monto: [0, [Validators.required, Validators.min(500)]],
       })
   }
 
@@ -35,9 +42,43 @@ export class RegisterPaymentsDialogComponent implements OnInit{
 
   closeDialog() {
     this.isVisible = false;
+
   }
 
   submitForm() {
     this.formValues.emit(this.registerPaymentsForm.value);
   }
+
+
+  validDateValidator(control: AbstractControl): ValidationErrors | null {
+
+    const inputDate = new Date(control.value);
+
+    if (isNaN(inputDate.getTime())) {
+      return { invalidDate: true };
+    }
+
+    return null;
+  }
+
+  setFormErrorMessage(error: ValidationErrors | null | undefined):string {
+
+    const formErrorMessages: ErrorType = {
+      'required': 'Este campo es requerido',
+      'min': 'El monto minimo es 500',
+      'invalidDate': 'Fecha Invalida',
+    };
+
+    const firstErrorKey = error ? Object.keys(error)[0] : null;
+
+    const errorMessage = error && firstErrorKey ? formErrorMessages[firstErrorKey] : '';
+
+    return  errorMessage;
+
+  }
+
+
+
+
+
 }
