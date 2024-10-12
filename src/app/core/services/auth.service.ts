071c2914075004470b654fab63f8bac26a146baf
SuelenGalhardo <suelenranucci@gmail.com>
2024-10-12 20:35:44 +0200

    added service tax oblitation

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -10,8 +10,8 @@ export class AuthService {
 
   private readonly refreshTokenUrl = ''; //Url para enviar el token de refresco y solicitar un nuevo token de acceso
   private refreshTokenInterval: any;
-  //private url = 'https://you-create-backend-service.onrender.com/';
-  private url = 'http://localhost:8000/';
+  private url = 'https://you-create-backend-service.onrender.com/';
+ // private url = 'http://localhost:8000/';
   private _currentUser = signal<any|null>(null);
 
   public currentUser = computed( () => this._currentUser() )
